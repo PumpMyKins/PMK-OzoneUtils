aedbcab8019e6ddd7d51690544d4c5057c055caa
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-06-29 18:16:32 +0200

    Ajout init method & ajout command executor

--- src/fr/pmk_ozone/island/IslandManager.java
+++ src/fr/pmk_ozone/island/IslandManager.java
@@ -1,7 +1,43 @@
 package fr.pmk_ozone.island;
 
+import fr.pmk_ozone.MainOzone;
+import fr.pmk_ozone.config.Config;
+
 public class IslandManager {
 
+	private static Config conf;
 	
+	public static IslandManager init(Config c) {
+		// TODO Auto-generated method stub
+		
+		conf = c;
+		
+		return new IslandManager();
+		
+	}
+
+	public static Config getConf() {
+		return conf;
+	}
+	
+	
+	private IslandCommandExecutor islandCmd;
+	
+	public IslandManager() {
+		
+		islandCmd = new IslandCommandExecutor();
+		
+		MainOzone.getInstance().getCommand("island").setExecutor(islandCmd);
+		MainOzone.getInstance().getCommand("is").setExecutor(islandCmd);
+		
+	}
+
+	public IslandCommandExecutor getIslandCmd() {
+		return islandCmd;
+	}
+
+	public void setIslandCmd(IslandCommandExecutor islandCmd) {
+		this.islandCmd = islandCmd;
+	}
 	
 }
