c6423669d21f34a45e201f68466bcbec25fd4d38
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-06-29 18:13:59 +0200

    Ajout class sub commande d'aide

---
+++ src/fr/pmk_ozone/island/commands/HelpIslandCmd.java
@@ -0,0 +1,14 @@
+package fr.pmk_ozone.island.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.entity.Player;
+
+public class HelpIslandCmd implements ISubCommand {
+
+	@Override
+	public boolean onSubCommand(Player sender, Command cmd, String[] args) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
