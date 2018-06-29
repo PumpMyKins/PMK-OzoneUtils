270d0efcfb3603dd3f69a52866baba60cc163cb1
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-06-29 17:04:52 +0200

    Ajout initialisation config

--- src/fr/pmk_ozone/MainOzone.java
+++ src/fr/pmk_ozone/MainOzone.java
@@ -2,8 +2,24 @@ package fr.pmk_ozone;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import fr.pmk_ozone.config.Config;
+
 public class MainOzone extends JavaPlugin {
 
+	public static MainOzone instance;
+	private static Config conf;
 	
+	@Override
+	public void onEnable() {
+		
+		instance = this;
+		conf = Config.getConfig(this);
+		
+		conf.initDataFolder();
+		conf.initAndGetFile("config.yml");
+		
+		
+		
+	}
 	
 }
