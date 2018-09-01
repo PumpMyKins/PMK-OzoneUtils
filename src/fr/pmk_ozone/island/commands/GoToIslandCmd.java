5e4fe1ab9a2ffc35e5a8dcd9227cf9e27508fbf9
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-09-01 22:18:11 +0200

    Update aide methode goto subCMD

--- src/fr/pmk_ozone/island/commands/GoToIslandCmd.java
+++ src/fr/pmk_ozone/island/commands/GoToIslandCmd.java
@@ -14,6 +14,11 @@ import com.sk89q.worldedit.bukkit.BukkitWorld;
 import fr.pmk_ozone.MainOzone;
 import fr.pmk_ozone.island.Island;
 import fr.pmk_ozone.island.IslandManager;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 
 public class GoToIslandCmd implements ISubCommand{
 
@@ -29,7 +34,7 @@ public class GoToIslandCmd implements ISubCommand{
 			File f = new File(MainOzone.getInstance().getDataFolder(),"islands.yml");
 			YamlConfiguration y = MainOzone.getConf().getConfiguration(f);
 			
-			Island island = Island.get(sender);
+			Island island = MainOzone.getIslandManager().getIsland(sender);
 			
 			sender.teleport(new Location(Bukkit.getWorld("world"), island.getSpawnX(), island.getSpawnY(), island.getSpawnZ()));
 			
@@ -52,8 +57,20 @@ public class GoToIslandCmd implements ISubCommand{
 	public void aide(Player p) {
 		// TODO Auto-generated method stub
 		
-		p.sendMessage("Sans ile, vous ne pouvez pas vous téléporter à celle-ci !");
-		p.sendMessage("Pour créer votre ile, cliquez ICI"); // clique = aide create island
+		p.sendMessage(Island.prefix + "§r§c Sans île, vous ne pouvez pas vous téléporter à celle-ci !");
+		
+		TextComponent ici = new TextComponent("ICI");
+		ici.setBold(true);
+		ici.setColor(ChatColor.DARK_AQUA);
+		ici.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "is create"));
+		
+		ici.setHoverEvent( new HoverEvent( HoverEvent.Action.SHOW_TEXT, new ComponentBuilder( "§3§lPour créer votre île !" ).create() ) );
+		
+		TextComponent msg = new TextComponent("Pour créer votre île, cliquez ");
+		msg.setColor(ChatColor.AQUA);
+		msg.addExtra(ici);
+		
+		p.spigot().sendMessage(msg); // clique = aide create island
 		
 	}
 	
