dbddfcfd61fc8c65cf43b7d510348dd5f68bc3f9
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-09-01 02:21:13 +0200

    Update aide methode GoTo subCMD

--- src/fr/pmk_ozone/island/commands/GoToIslandCmd.java
+++ src/fr/pmk_ozone/island/commands/GoToIslandCmd.java
@@ -5,13 +5,27 @@ import java.util.List;
 import org.bukkit.command.Command;
 import org.bukkit.entity.Player;
 
+import fr.pmk_ozone.MainOzone;
+import fr.pmk_ozone.island.IslandManager;
+
 public class GoToIslandCmd implements ISubCommand{
 
 	@Override
 	public boolean onSubCommand(Player sender, Command cmd, List<String> args) {
 		
-		sender.sendMessage("go to commande");		
-		return true;
+		IslandManager is = MainOzone.getIslandManager();
+		
+		if(is.playerHasIsland(sender)) {
+			// téléportation
+			System.out.println("téléportation");			
+		}else {
+			// affichage aide
+			aide(sender);
+			
+		}
+		
+		return false;
+		
 	}
 
 	public boolean onSubCommand(Player p, Command cmd) {
@@ -22,6 +36,9 @@ public class GoToIslandCmd implements ISubCommand{
 	@Override
 	public void aide(Player p) {
 		// TODO Auto-generated method stub
+		
+		p.sendMessage("Sans ile, vous ne pouvez pas vous téléporter à celle-ci !");
+		p.sendMessage("Pour créer votre ile, cliquez ICI"); // clique = aide create island
 		
 	}
 	
