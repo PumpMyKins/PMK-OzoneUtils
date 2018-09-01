9f75a482fc1310cd28e2729f502e194b5b897911
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-09-01 20:11:34 +0200

    Fix help subCMD return null message

--- src/fr/pmk_ozone/island/commands/HelpIslandCmd.java
+++ src/fr/pmk_ozone/island/commands/HelpIslandCmd.java
@@ -7,6 +7,8 @@ import java.util.List;
 import org.bukkit.command.Command;
 import org.bukkit.entity.Player;
 
+import net.minecraft.util.io.netty.handler.codec.compression.SnappyFramedEncoder;
+
 public class HelpIslandCmd implements ISubCommand {
 
 	private static List<String> helpList = new ArrayList<String>();
@@ -18,7 +20,9 @@ public class HelpIslandCmd implements ISubCommand {
 		
 		sender.sendMessage("§3§l=======§r§b PumpMyAide§r§3§l =======");
 		
-		for (String string : args) {
+		for (String string : helpList) {
+			
+			sender.sendMessage(string);
 			
 		}
 		
