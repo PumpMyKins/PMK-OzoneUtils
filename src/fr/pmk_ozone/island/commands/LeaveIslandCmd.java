ce9096684b948930629dd85147f1f225aa87cb98
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-09-03 13:40:16 +0200

    Update leave subCMD

--- src/fr/pmk_ozone/island/commands/LeaveIslandCmd.java
+++ src/fr/pmk_ozone/island/commands/LeaveIslandCmd.java
@@ -1,9 +1,9 @@
 package fr.pmk_ozone.island.commands;
 
+import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
 
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.entity.Player;
 
@@ -23,12 +23,49 @@ public class LeaveIslandCmd implements ISubCommand {
 			
 			Island island = MainOzone.getIslandManager().getIsland(sender);
 			
-			sender.teleport(new Location(Bukkit.getWorld("world"), island.getSpawnX(), island.getSpawnY(), island.getSpawnZ()));
+			if(island.getOwnerUUID().equals(sender.getUniqueId().toString())) {
+				
+				if(island.getPlayerList().size() != 0) {
+					
+					//impossible de quitter
+					aide1(sender);
+					return true;
+					
+				}				
+				
+				island.setOwnerUUID("none");
+				
+			}
+			//quitter
+			sender.sendMessage(Island.prefix + "§r§d Vous venez de quitter votre île !");
+			
+			for (Iterator<String> i = island.getPlayerList().iterator(); i.hasNext();) {
+				
+				if(i.next().equals(sender.getUniqueId().toString()))
+					i.remove();
+				
+			}
+			
+			try {
+				island.save();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			try {
+				IslandManager.unsetIsland(sender);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
 			return true;
 			
 		}else {
 			// affichage aide
 			aide(sender);
+			new GoToIslandCmd().aide1(sender);
 			return false;
 			
 		}
@@ -38,7 +75,12 @@ public class LeaveIslandCmd implements ISubCommand {
 	@Override
 	public void aide(Player p) {
 		// TODO Auto-generated method stub
-
+		p.sendMessage(Island.prefix + "§r§c Vous devez avoir une île pour pouvoir la quitter !");
+	}
+	
+	public void aide1(Player p) {
+		// TODO Auto-generated method stub
+		p.sendMessage(Island.prefix + "§r§c Vous etes le créateur de cette ile, ne laissez pas tomber vos coéquipiers !");
 	}
 
 }
