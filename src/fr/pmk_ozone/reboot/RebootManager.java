083d4af59ee27d98bd9eb8e2ad9c06468a73f1dc
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-09-04 16:39:06 +0200

    Creation class rebootManager

---
+++ src/fr/pmk_ozone/reboot/RebootManager.java
@@ -0,0 +1,23 @@
+package fr.pmk_ozone.reboot;
+
+import java.util.List;
+
+public class RebootManager {
+
+	private static List<String> getRebootList(){
+		
+		return null;
+		
+	}
+	
+	private
+	
+	public static void init() {
+		
+		
+		
+	}
+	
+	
+	
+}
