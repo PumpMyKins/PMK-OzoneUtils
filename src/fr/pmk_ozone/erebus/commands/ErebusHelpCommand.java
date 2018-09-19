8e448d1f116ba2be6c45cf2d2c718c7eaa22ffe7
Sufike <l.christ54260@gmail.com>
2018-09-19 19:39:57 +0200

    add final Help Command

--- src/fr/pmk_ozone/erebus/commands/ErebusHelpCommand.java
+++ src/fr/pmk_ozone/erebus/commands/ErebusHelpCommand.java
@@ -11,7 +11,11 @@ public class ErebusHelpCommand implements ISubCommand{
 
 	@Override
 	public boolean onSubCommand(Player sender, Command cmd, List<String> args) {
-		// TODO Auto-generated method stub
+		
+		sender.sendMessage("§6§l[§r§2PumpMyErebus§6§l] §r§2§oListe des Commandes §r§6§l[§r§2PumpMyKit§6§l]");
+		sender.sendMessage("§2 /erebus boss");
+		sender.sendMessage("§2 /erebus mobs");
+		
 		return false;
 	}
 
