3fcd3392498655004a16670cc46cfa8c17bba67f
Sufike <l.christ54260@gmail.com>
2018-09-19 22:20:24 +0200

    GemAPI

--- src/fr/pmk_ozone/erebus/commands/ErebusBossCommand.java
+++ src/fr/pmk_ozone/erebus/commands/ErebusBossCommand.java
@@ -5,13 +5,32 @@ import java.util.List;
 import org.bukkit.command.Command;
 import org.bukkit.entity.Player;
 
-import fr.pmk_ozone.erebus.commands.ISubCommand;;
+import fr.pmk_ozone.erebus.commands.ISubCommand;
+import me.xanium.gemseconomy.api.GemsEconomyAPI;
 
 public class ErebusBossCommand implements ISubCommand{
 
+
 	@Override
 	public boolean onSubCommand(Player sender, Command cmd, List<String> args) {
-		// TODO Auto-generated method stub
+		
+		GemsEconomyAPI GemApi = new GemsEconomyAPI();
+
+		if(sender.getWorld().getName().endsWith("DIM66")) {
+			
+			System.out.println("Sucess");
+			
+			
+		}
+		else {
+			/* DEBUG
+			*System.out.println("Retry");
+			*System.out.println(sender.getWorld().getName());
+			*
+			*
+			*/
+		}
+		
 		return false;
 	}
 
