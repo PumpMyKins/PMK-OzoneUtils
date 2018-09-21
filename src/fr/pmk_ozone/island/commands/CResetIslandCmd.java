5657b40fb175bdb0f6ad06caa802d5220d24c437
Clem-Fern <clement.coussieu@gmail.com>
2018-09-21 23:44:31 +0200

    Ajout basic check/conditions confirm subcmd executor

--- src/fr/pmk_ozone/island/commands/CResetIslandCmd.java
+++ src/fr/pmk_ozone/island/commands/CResetIslandCmd.java
@@ -5,12 +5,31 @@ import java.util.List;
 import org.bukkit.command.Command;
 import org.bukkit.entity.Player;
 
+import fr.pmk_ozone.MainOzone;
+import fr.pmk_ozone.island.Island;
+import fr.pmk_ozone.island.IslandManager;
+
 public class CResetIslandCmd implements ISubCommand {
 
 	@Override
 	public boolean onSubCommand(Player sender, Command cmd, List<String> args) {
-		// TODO Auto-generated method stub
-		return false;
+		
+		IslandManager is = MainOzone.getIslandManager();
+		
+		if(is.playerHasIsland(sender)) {
+			
+			Island island = MainOzone.getIslandManager().getIsland(sender);
+			
+			// reset de l'island
+			
+			return true;
+			
+		}else {
+			
+			new ResetIslandCmd().aide(sender);		
+			return true;
+		}
+		
 	}
 
 	@Override
