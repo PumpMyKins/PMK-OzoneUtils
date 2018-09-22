908752b98b75af9e2beeabf0317475762411147a
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-09-23 01:33:22 +0200

    Fix parameters names

--- src/fr/pmk_ozone/command/StartingStuffCommand.java
+++ src/fr/pmk_ozone/command/StartingStuffCommand.java
@@ -12,7 +12,7 @@ public class StartingStuffCommand implements CommandExecutor {
 	private static HashMap<String, Timestamp> hashStartHash = new HashMap<>();
 	
 	@Override
-	public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
+	public boolean onCommand(CommandSender sender, Command cmd, String arg, String[] args) {
 		// TODO Auto-generated method stub
 		return false;
 	}
