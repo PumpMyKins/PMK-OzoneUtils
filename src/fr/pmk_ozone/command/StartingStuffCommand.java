e0e1953199666d1ea08f1e36c83f3d34fdb855b2
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-09-23 10:59:17 +0200

    Fix inventory size exception

--- src/fr/pmk_ozone/command/StartingStuffCommand.java
+++ src/fr/pmk_ozone/command/StartingStuffCommand.java
@@ -54,7 +54,7 @@ public class StartingStuffCommand implements CommandExecutor {
 		}
 		
 		// execution de la commande
-		Inventory inv = Bukkit.createInventory(null, 6, ChatColor.BLUE + "" + ChatColor.BOLD + "Stuff de départ");
+		Inventory inv = Bukkit.createInventory(null, 9, ChatColor.BLUE + "" + ChatColor.BOLD + "Stuff de départ");
 		
 		// ajout des items
 		
