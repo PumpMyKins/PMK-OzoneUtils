d80c81eb7483e107cf13adc7e40f79b734b09049
Sufike <l.christ54260@gmail.com>
2018-10-30 22:49:31 +0100

    fix decimal money command

--- src/fr/pmk_ozone/command/MoneyCommand.java
+++ src/fr/pmk_ozone/command/MoneyCommand.java
@@ -1,5 +1,8 @@
 package fr.pmk_ozone.command;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -14,13 +17,20 @@ public class MoneyCommand implements CommandExecutor{
 		GemsEconomyAPI apiEco = new GemsEconomyAPI();
 		Player sender = (Player) cmdSender;
 		if(sender instanceof Player) {
-			double balance =  apiEco.getBalance(sender.getUniqueId());
+			double balance = round(apiEco.getBalance(sender.getUniqueId()), 2);
 			sender.sendMessage("§6§l[§r§2PumpMyEconomy§6§l]§r§2Vous avez §r§4"+ balance +" §r§6PumpCoins");
 		}
 		else {
 			System.out.println("You are not a player");
 		}
 		return false;
+	}
+	public static double round(double value, int places) {
+	    if (places < 0) throw new IllegalArgumentException();
+
+	    BigDecimal bd = new BigDecimal(value);
+	    bd = bd.setScale(places, RoundingMode.HALF_UP);
+	    return bd.doubleValue();
 	}
 
 	
